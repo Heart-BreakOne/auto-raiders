e62b2c746c88846eec8d5d6e07759b43ed590ddf
Heart-BreakOne <lcoutinholuiz@gmail.com>
2023-11-04 07:09:10 -0300

    Wipe captain mode list

--- pause_handler.js
+++ pause_handler.js
@@ -2,10 +2,11 @@
 let pause = String.fromCharCode(9208);
 let play = String.fromCharCode(9654);
 let pauseArray = [];
+let newButton
 
 function injectIntoDOM() {
     // Find all elements with class 'capSlotNameCont'
-    var capSlotNameContList = document.querySelectorAll('.capSlotNameCont');
+    let capSlotNameContList = document.querySelectorAll('.capSlotNameCont');
 
     // Iterate through the list of elements
     capSlotNameContList.forEach(function (capSlotNameCont) {
@@ -14,7 +15,7 @@ function injectIntoDOM() {
 
         if (!existingElement) {
             // Create a new button element
-            var newButton = document.createElement('div');
+            newButton = document.createElement('div');
             newButton.classList.add('pauseButton');
             newButton.innerText = play; // Set the button text
             newButton.style.paddingLeft = "40px";
@@ -24,14 +25,38 @@ function injectIntoDOM() {
         }
 
     });
+
+    // Create a new button element
+    let existingButton = document.querySelector('.wipeButton')
+    if (!existingButton) {
+        // Create a new button element
+        // Create a new button element
+        newButton = document.createElement('button');
+        newButton.className = 'wipeButton';
+        newButton.innerHTML = 'Wipe<br>captain';
+        newButton.style.backgroundColor = "#5fa695";
+        newButton.style.height = "auto";
+        newButton.style.marginRight = "15px";
+        newButton.style.width = "auto";
+        newButton.style.color = "white";
+        newButton.style.padding = "15px";
+        newButton.style.fontSize = "18px";
+
+        // Find the quantityItemsCont element
+        var quantityItemsCont = document.querySelector('.quantityItemsCont');
+
+        // Insert the new button before the quantityItem element
+        quantityItemsCont.insertBefore(newButton, quantityItemsCont.firstChild);
+
+    }
 }
 
 
 document.addEventListener('click', function (event) {
     if (event.target.classList.contains('pauseButton')) {
-        var button = event.target;
-        var capSlotNameCont = event.target.closest('.capSlotNameCont');
-        var captainName = capSlotNameCont.querySelector('.capSlotName > div').innerText;
+        let button = event.target;
+        let capSlotNameCont = event.target.closest('.capSlotNameCont');
+        let captainName = capSlotNameCont.querySelector('.capSlotName > div').innerText;
         if (button.innerText == pause) {
             //Play
             button.innerText = play;
@@ -42,8 +67,13 @@ document.addEventListener('click', function (event) {
             saveStateToStorage(captainName, true)
         }
     }
-});
 
+    if (event.target.classList.contains('wipeButton')) {
+        chrome.storage.local.remove(['dungeonCaptain']);
+        chrome.storage.local.remove(['clashCaptain']);
+        chrome.storage.local.remove(['duelCaptain']);
+    }
+});
 
 var dataArray = [];
 
