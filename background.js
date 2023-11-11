dd6a3695a4b20069ff2e766656b60f43b4289a97
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-11-11 18:17:48 -0300

    Fixing agent spoofer

--- background.js
+++ background.js
@@ -27,94 +27,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   }
 });
 
-
-// background.js
-
-'use strict';
-
-const context = () => chrome.storage.local.get({
-  'mode': 'android'
-}, prefs => {
-  chrome.contextMenus.create({
-    title: 'Type: Android',
-    id: 'android',
-    type: 'radio',
-    checked: prefs.mode === 'android',
-    contexts: ['action']
-  }, () => chrome.runtime.lastError);
-  chrome.contextMenus.create({
-    title: 'Type: iOS',
-    id: 'ios',
-    type: 'radio',
-    checked: prefs.mode === 'ios',
-    contexts: ['action']
-  }, () => chrome.runtime.lastError);
-  chrome.contextMenus.create({
-    title: 'Type: Kindle',
-    id: 'kindle',
-    type: 'radio',
-    checked: prefs.mode === 'kindle',
-    contexts: ['action']
-  }, () => chrome.runtime.lastError);
-  chrome.contextMenus.create({
-    title: 'Test my User-Agent',
-    id: 'test-ua',
-    contexts: ['action']
-  }, () => chrome.runtime.lastError);
-});
-chrome.runtime.onStartup.addListener(context);
-chrome.runtime.onInstalled.addListener(context);
-
-chrome.contextMenus.onClicked.addListener(info => {
-    chrome.storage.local.set({
-      mode: info.menuItemId
-    });
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get({
-    'mode': 'android',
-    'ua-android': 'Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-    'ua-ios': 'Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/19A346 Safari/602.1',
-    'ua-kindle': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.146.3-Gen4_12000410) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true',
-    'bypass-cache': false
-  }, async prefs => {
-    const rules = await chrome.declarativeNetRequest.getSessionRules();
-    if (rules.some(r => r.id === tab.id)) {
-      await chrome.declarativeNetRequest.updateSessionRules({
-        removeRuleIds: [tab.id]
-      });
-    }
-    else {
-      await chrome.declarativeNetRequest.updateSessionRules({
-        removeRuleIds: [tab.id],
-        addRules: [{
-          'id': tab.id,
-          'action': {
-            'type': 'modifyHeaders',
-            'requestHeaders': [{
-              'header': 'user-agent',
-              'operation': 'set',
-              'value': prefs['ua-' + prefs.mode]
-            }]
-          },
-          'condition': {
-            'tabIds': [tab.id],
-            'resourceTypes': [
-              'main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'font', 'object', 'xmlhttprequest', 'ping',
-              'csp_report', 'media', 'websocket', 'webtransport', 'webbundle', 'other'
-            ]
-          }
-        }]
-      });
-    }
-    chrome.tabs.reload(tab.id, {
-      bypassCache: prefs['bypass-cache']
-    });
-  });
-});
 // Store the tab IDs that have already been processed
 const processedTabs = new Set();
+
+// Define the static user agent
+const staticUserAgent = 'Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36';
 
 // This function handles the logic to update session rules and reload the tab
 async function updateUserAgent(tab) {
@@ -124,16 +41,6 @@ async function updateUserAgent(tab) {
   }
 
   processedTabs.add(tab.id);
-
-  const prefs = await new Promise(resolve => {
-    chrome.storage.local.get({
-      'mode': 'android',
-      'ua-android': 'Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-      'ua-ios': 'Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/19A346 Safari/602.1',
-      'ua-kindle': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.146.3-Gen4_12000410) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true',
-      'bypass-cache': false
-    }, resolve);
-  });
 
   const rules = await chrome.declarativeNetRequest.getSessionRules();
   if (rules.some(r => r.id === tab.id)) {
@@ -150,7 +57,7 @@ async function updateUserAgent(tab) {
           'requestHeaders': [{
             'header': 'user-agent',
             'operation': 'set',
-            'value': prefs['ua-' + prefs.mode]
+            'value': staticUserAgent
           }]
         },
         'condition': {
@@ -164,7 +71,7 @@ async function updateUserAgent(tab) {
     });
   }
   chrome.tabs.reload(tab.id, {
-    bypassCache: prefs['bypass-cache']
+    bypassCache: false // You can set this to true or false based on your requirements
   });
 }
 
