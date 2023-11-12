33d3e57dac2828a62ab6ffb2d72f1892dc7590e1
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-11-12 00:23:28 -0300

    Wipe button

--- options.js
+++ options.js
@@ -15,5 +15,16 @@ document.addEventListener('DOMContentLoaded', function () {
             entryElement.innerHTML = `Slot: ${entry.logId} - Captain Name: ${entry.logCapName} - Game Mode: ${entry.logMode} - Result: ${entry.result} - Time taken: ${entry.elapsedTime} minutes`;
             dataContainer.appendChild(entryElement);
         });
+
+        // Add event listener for the wipe button
+        document.getElementById('wipeButton').addEventListener('click', function () {
+            // Wipe logData from local storage
+            chrome.storage.local.remove(['logData'], function () {
+                console.log('logData wiped from local storage');
+                // Optionally, you can also clear the displayed data on the options page
+                dataContainer.innerHTML = '';
+            });
+        });
+
     });
 });
