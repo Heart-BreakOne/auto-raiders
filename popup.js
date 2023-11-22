b77f9ff8515a745a1ba9b68a0308ac137221fa3b
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-11-21 21:16:03 -0300

    Organizing files

--- popup.js
+++ popup.js
@@ -9,13 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
     //Event listener for a button to open the options page of the extension (log.html)
     document.getElementById("log_button").addEventListener('click', function () {
         // Open the options page
-        const url = `chrome-extension://${chrome.runtime.id}/log.html`
+        const url = `chrome-extension://${chrome.runtime.id}/html/log.html`
         chrome.tabs.create({ url: url });
     });
 
     document.getElementById("whitelist_button").addEventListener('click', function () {
         // Open the options page
-        const url = `chrome-extension://${chrome.runtime.id}/whitelist.html`
+        const url = `chrome-extension://${chrome.runtime.id}/html/whitelist.html`
         chrome.tabs.create({ url: url });
     });
 
