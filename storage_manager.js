8b2cef92995c21533879a0d19b5101070a22af5e
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-02-10 22:03:08 -0300

    Async tasks

--- storage_manager.js
+++ storage_manager.js
@@ -5,7 +5,7 @@
 
 //This function receives a key which is a string with their game mode and a value which is the captain's name.
 //The key is unique so only one captain can exist running that mode.
-function saveToStorage(key, value) {
+async function saveToStorage(key, value) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.set({ [key]: value }, function () {
             if (chrome.runtime.lastError) {
@@ -18,7 +18,7 @@ function saveToStorage(key, value) {
 }
 
 //Receives the game mode key and returns the name of the captain currently running it.
-function retrieveFromStorage(key) {
+async function retrieveFromStorage(key) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get([key], function (result) {
             resolve(result[key]);
@@ -26,7 +26,7 @@ function retrieveFromStorage(key) {
     });
 }
 
-function retrieveNumberFromStorage(key) {
+async function retrieveNumberFromStorage(key) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get(key, (result) => {
             if (chrome.runtime.lastError) {
