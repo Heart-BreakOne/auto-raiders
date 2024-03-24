239439aa447b527e0fe2d0d638cd4840b71bb451
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-03-24 14:57:21 -0300

    Fix for dungeon slot manager

--- dungeon_slot_manager.js
+++ dungeon_slot_manager.js
@@ -7,5 +7,32 @@ async function manageDungeonSlot() {
     if (!slot0) {
         return
     }
-    
+
+    // Attempt to clear other slots doing dungeons
+    let slots = document.querySelectorAll(".capSlot")
+    for (let i = 1; i < slots.length; i++) {
+        let slot = slots[i];
+        if (slot.innerHTML.includes("Dungeons")) {
+            slot.querySelector(".fal.fa-times-square")?.click();
+        }
+    }
+    let firstSlot = slots[0]
+    if (firstSlot) {
+        const closeBtn = firstSlot.querySelector(".fal.fa-times-square")
+
+        if (!firstSlot.innerHTML.includes("Dungeons") && closeBtn) {
+            closeBtn.click()
+        } else if (!firstSlot.innerHTML.includes("Dungeons")) {
+            chrome.storage.local.get("offlinePermission", function (data) {
+                data.offlinePermission.offlineButton_1 = 0;
+                chrome.storage.local.set({ "offlinePermission": data.offlinePermission });
+            });
+        }
+
+
+    }
+    firstSlot = document.querySelector(".capSlot")
+
+
+
 }
