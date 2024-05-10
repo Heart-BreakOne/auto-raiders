b40500126deefeffc4b846adccd1e7e8424cd401
heather1209 <heatherleigh@gmail.com>
2024-05-09 21:49:30 -0400

    updated chest purchaser to switch from bones/keys to rubies

--- collector.js
+++ collector.js
@@ -277,8 +277,7 @@ async function buyChests() {
     let currentUserCurrencies = await retrieveFromStorage("availableCurrencies");
     if (!currentUserCurrencies || !currentUserCurrencies.data || currentUserCurrencies.data == undefined) return;
 
-    let bones = currentUserCurrencies.data.bones;
-    let keys = currentUserCurrencies.data.keys;
+    let rubies = currentUserCurrencies.data.rubies;
 
     let buyCheapestFirst = await retrieveFromStorage("chestPurchaseOrder");
 
@@ -335,11 +334,9 @@ async function buyChests() {
 
     let buyAllSkins = await retrieveFromStorage("buyAllSkins");
     if (buyAllSkins) {
-        await buyChestsWithSkins(keys, await retrieveFromStorage("buyThisKeyChest"), await retrieveNumberFromStorage("minKeyCurrency"), "dungeonChestsData");
-        await buyChestsWithSkins(bones, await retrieveFromStorage("buyThisBoneChest"), await retrieveNumberFromStorage("minBoneCurrency"), "boneChestsData");
+        await buyChestsWithSkins(rubies, await retrieveFromStorage("buyThisRubyChest"), await retrieveNumberFromStorage("minRubyCurrency"), "rubyChestsData");
     } else {
-        await buyChestsWithCurrency(keys, await retrieveNumberFromStorage("minKeyCurrency"), "dungeonChestsData");
-        await buyChestsWithCurrency(bones, await retrieveNumberFromStorage("minBoneCurrency"), "boneChestsData");
+        await buyChestsWithCurrency(rubies, await retrieveNumberFromStorage("minRubyCurrency"), "rubyChestsData");
     }
 
 }
--- javascript/chests.js
+++ javascript/chests.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     await loadChestData();
 
-    await loadDropDownMenu("dungeonChestsData", "buy_one_key_skin");
-    await loadDropDownMenu("boneChestsData", "buy_one_bone_skin");
+    await loadDropDownMenu("rubyChestsData", "buy_one_ruby_skin");
 
     await initializeSwitch("chestPurchaseOrder");
     await initializeSwitch("buyAllSkins");
     await initializeSwitch("skinChestFocus");
 
-    await loadSelects("buyThisBoneChest", "buy_one_bone_skin");
-    await loadSelects("buyThisKeyChest", "buy_one_key_skin");
+    await loadSelects("buyThisRubyChest", "buy_one_ruby_skin");
 
     document.getElementById('fetch_btn').addEventListener('click', function () {
         fetchAndSaveChestData();
@@ -48,19 +46,11 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 async function savePreferences() {
-    let bc = document.getElementById('min_bone_currency').value;
-    await chrome.storage.local.set({ 'minBoneCurrency': bc });
+    let rc = document.getElementById('min_ruby_currency').value;
+    await chrome.storage.local.set({ 'minRubyCurrency': rc });
 
-    let kc = document.getElementById('min_key_currency').value;
-    await chrome.storage.local.set({ 'minKeyCurrency': kc });
-
-
-    let b_sel = document.getElementById('buy_one_bone_skin').value;
-    await chrome.storage.local.set({ 'buyThisBoneChest': b_sel });
-
-    let k_sel = document.getElementById('buy_one_key_skin').value;
-    await chrome.storage.local.set({ 'buyThisKeyChest': k_sel });
-
+    let r_sel = document.getElementById('buy_one_ruby_skin').value;
+    await chrome.storage.local.set({ 'buyThisRubyChest': r_sel });
 }
 
 async function fetchAndSaveChestData() {
@@ -104,19 +94,15 @@ async function fetchAndSaveChestData() {
             return filteredChests;
         };
 
-        let freshDungeonArray = filterChests("Dungeon");
-        let freshBoneArray = filterChests("Bones");
+        let freshRubyArray = filterChests("Rubies");
 
         let chests = gameData.sheets.Chests;
         let rewards = gameData.sheets.ChestRewardSlots;
 
-        freshDungeonArray = appendDisplayName(freshDungeonArray, chests);
-        freshDungeonArray = appendSkins(freshDungeonArray, chests, rewards);
-        freshBoneArray = appendDisplayName(freshBoneArray, chests);
-        freshBoneArray = appendSkins(freshBoneArray, chests, rewards);
+        freshRubyArray = appendDisplayName(freshRubyArray, chests);
+        freshRubyArray = appendSkins(freshRubyArray, chests, rewards);
 
-        await chrome.storage.local.set({ 'dungeonChestsData': freshDungeonArray });
-        await chrome.storage.local.set({ 'boneChestsData': freshBoneArray });
+        await chrome.storage.local.set({ 'rubyChestsData': freshRubyArray });
 
         location.reload();
 
@@ -147,14 +133,12 @@ async function updateCurrency(key, id) {
 }
 
 async function loadChestData() {
-    await updateCurrency("minBoneCurrency", "min_bone_currency");
-    await updateCurrency("minKeyCurrency", "min_key_currency");
+    await updateCurrency("minRubyCurrency", "min_ruby_currency");
 
-    let dcd = await retrieveFromStorage("dungeonChestsData");
-    let bcd = await retrieveFromStorage("boneChestsData");
-    if (!dcd || !bcd) return;
+    let rcd = await retrieveFromStorage("rubyChestsData");
+    if (!rcd) return;
     let userChests = await retrieveFromStorage("userChests");
-    let mdc = dcd.concat(bcd);
+    let mdc = rcd;
     let chestContainer = document.getElementById("chest_container");
     let table = document.createElement("table");
 
